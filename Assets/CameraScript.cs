b320f04b3c70272e702b2d073dc88ec4dc8ef485
sairam4123 <sairamkumar2022@gmail.com>
2020-11-24 19:15:56 +0530

    Fix some bugs

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Timeline;
 
 public class CameraScript : MonoBehaviour
 {
@@ -10,67 +11,67 @@ public class CameraScript : MonoBehaviour
             if (Input.GetKey(KeyCode.UpArrow))
             {
                 var eulerAngles = transform.rotation.eulerAngles;
-                eulerAngles += Vector3.left;
+                eulerAngles += Vector3.left * Time.deltaTime;
                 var quaternion = Quaternion.Euler(eulerAngles);
                 transform.rotation = quaternion;
             }
             else if (Input.GetKey(KeyCode.DownArrow))
             {
                 var eulerAngles = transform.rotation.eulerAngles;
-                eulerAngles += Vector3.right;
+                eulerAngles += Vector3.right * Time.deltaTime;
                 var quaternion = Quaternion.Euler(eulerAngles);
                 transform.rotation = quaternion;
             }
             else if (Input.GetKey(KeyCode.LeftArrow))
             {
                 var eulerAngles = transform.rotation.eulerAngles;
-                eulerAngles += Vector3.up;
+                eulerAngles += Vector3.up * Time.deltaTime;
                 var quaternion = Quaternion.Euler(eulerAngles);
                 transform.rotation = quaternion;
             }
             else if (Input.GetKey(KeyCode.RightArrow))
             {
                 var eulerAngles = transform.rotation.eulerAngles;
-                eulerAngles += Vector3.down;
+                eulerAngles += Vector3.down * Time.deltaTime;
                 var quaternion = Quaternion.Euler(eulerAngles);
                 transform.rotation = quaternion;
             }
             else if (Input.GetKey(KeyCode.PageUp))
             {
-                transform.localPosition += Vector3.forward;
+                transform.localPosition += Vector3.forward * Time.deltaTime;
             }
             else if (Input.GetKey(KeyCode.PageDown))
             {
-                transform.localPosition += Vector3.back;
+                transform.localPosition += Vector3.back * Time.deltaTime;
             }
         }
         else
         {
             if (Input.GetKey(KeyCode.UpArrow))
             {
-                transform.Translate(Vector3.up);
+                transform.Translate(Vector3.up * Time.deltaTime);
             }
             else if (Input.GetKey(KeyCode.DownArrow))
             {
-                transform.Translate(Vector3.down);
+                transform.Translate(Vector3.down * Time.deltaTime);
             }
 
             if (Input.GetKey(KeyCode.LeftArrow))
             {
-                transform.Translate(Vector3.left);
+                transform.Translate(Vector3.left * Time.deltaTime);
             }
             else if (Input.GetKey(KeyCode.RightArrow))
             {
-                transform.Translate(Vector3.right);
+                transform.Translate(Vector3.right * Time.deltaTime);
             }
 
             if (Input.GetKey(KeyCode.PageUp))
             {
-                transform.Translate(Vector3.forward);
+                transform.Translate(Vector3.forward * Time.deltaTime);
             }
             else if (Input.GetKey(KeyCode.PageDown))
             {
-                transform.Translate(Vector3.back);
+                transform.Translate(Vector3.back * Time.deltaTime);
             }
 
             if (Input.GetKey(KeyCode.W))
@@ -103,7 +104,8 @@ public class CameraScript : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Mouse0))
         {
-            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * (Time.deltaTime * 200f));
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) *
+                             (Time.deltaTime * 200f));
         }
     }
 }
